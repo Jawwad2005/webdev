f26ce194a8952f15a29fe54d3703d745dd13ae2e
Jawwad2005 <jawwadfc@gmail.com>
2025-01-16 23:24:57 +0530

    Stack complete final

--- stackscript.js
+++ stackscript.js
@@ -10,18 +10,22 @@ document.addEventListener("DOMContentLoaded", function () {
 
 
             const div = document.createElement("div"); 
+            div.style.color = "white";
             div.style.background = "gray";
-            div.style.border = "2px solid black";
-            div.style.width = "50px";
+            div.style.border = "2px solid lightgray";
+            div.style.borderRadius = "3px";
+            div.style.height = "30px"
+            div.style.width = "100px";
             div.style.margin = "1px";
+            div.style.display = "flex";
+            div.style.justifyContent = "center";
+            div.style.alignItems = "center";
             div.innerHTML = input;
-            document.getElementById("container").appendChild(div);
-
-        
+            document.getElementById("stack").appendChild(div);
     });
 
     document.getElementById("pop").addEventListener("click", function(){
-        const container = document.getElementById("container");
+        const container = document.getElementById("stack");
 
         document.getElementById("popped").innerHTML 
         = "Current popped element: " + container.lastChild.innerHTML;
