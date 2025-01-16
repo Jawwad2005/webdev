96c4af92b32ba0fd27522d7656d48d15d4ea0569
Jawwad2005 <jawwadfc@gmail.com>
2025-01-16 23:23:33 +0530

    Stack complete

--- stackscript.js
+++ stackscript.js
@@ -1,6 +1,33 @@
+
+
 document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("submit").addEventListener("click", function () {
+
+    
+
+    document.getElementById("push").addEventListener("click", function () {
+
         const input = document.getElementById("elems").value;
-        alert(input);
+
+
+            const div = document.createElement("div"); 
+            div.style.background = "gray";
+            div.style.border = "2px solid black";
+            div.style.width = "50px";
+            div.style.margin = "1px";
+            div.innerHTML = input;
+            document.getElementById("container").appendChild(div);
+
+        
+    });
+
+    document.getElementById("pop").addEventListener("click", function(){
+        const container = document.getElementById("container");
+
+        document.getElementById("popped").innerHTML 
+        = "Current popped element: " + container.lastChild.innerHTML;
+
+        if(container.lastChild){
+            container.removeChild(container.lastChild);
+        }
     });
 });
